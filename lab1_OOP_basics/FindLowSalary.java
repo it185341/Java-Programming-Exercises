bbf8ae3027ed205644b48e43de6d5745db773ed0
Maria Moumoulidou <149332478+it185341@users.noreply.github.com>
2025-08-06 20:06:28 +0300

    Update FindLowSalary.java

--- lab1_OOP_basics/FindLowSalary.java
+++ lab1_OOP_basics/FindLowSalary.java
@@ -1,1 +1,28 @@
-
+public class TestEmployee {
+    public static void FindLowSalary(Employee E1, Employee E2){
+        double e1 = E1.salary();
+        double e2 = E2.salary();
+        if(e1<1200 && E1.getyears() >= 5){
+            System.out.println("O ergazomenos "+E1.getfullName()+" einai xamilomisthos");
+            E1.setbaseSalary(E1.getbaseSalary() + 100);
+            System.out.println("Nea stoixeia: \n" + E1);
+            System.out.println("============================================================");
+        }
+        if(e2<1200 && E2.getyears() >= 5){
+            System.out.println("O ergazomenos "+E2.getfullName()+" einai xamilomisthos");
+            E1.setbaseSalary(E2.getbaseSalary() + 100);
+            System.out.println("Nea stoixeia: \n" + E2);
+            System.out.println("=============================================================");
+        }
+    }
+    public static void main(String[] args) {
+        Employee E1= new Employee (12345, "Moumoulidou Maria", 900 , 5.0 , 100);
+        Employee E2= new Employee (23456, "Andritsoudi Despoina", 1300 , 2.5 , 50);
+        System.out.println(E1);
+        System.out.println();
+        System.out.println(E2);
+        System.out.println();
+        System.out.println("=============================================================");
+        FindLowSalary(E1,E2);
+    }
+}
