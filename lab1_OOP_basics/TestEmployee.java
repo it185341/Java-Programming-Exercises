2c52985f2eb334baac03d5178d939e385cc4c922
Maria Moumoulidou <149332478+it185341@users.noreply.github.com>
2025-08-06 19:49:52 +0300

    Create TestEmployee.java

---
+++ lab1_OOP_basics/TestEmployee.java
@@ -0,0 +1,8 @@
+public static void main(String[] args) {
+        Employee E1= new Employee (12345, "Employee 1", 900 , 5.0 , 100); // Create one Employee object E1
+        Employee E2= new Employee (23456, "Employee 2", 1300 , 2.5 , 50); // Create one Employee object E2
+        
+        // Print employee information using toString()
+        System.out.println(E1);
+        System.out.println(E2);
+}
