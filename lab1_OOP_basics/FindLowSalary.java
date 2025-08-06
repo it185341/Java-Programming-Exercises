b6c95e3c1a6d8957338b04549d88b8b22f81c3a7
Maria Moumoulidou <149332478+it185341@users.noreply.github.com>
2025-08-06 20:09:10 +0300

    Update FindLowSalary.java

--- lab1_OOP_basics/FindLowSalary.java
+++ lab1_OOP_basics/FindLowSalary.java
@@ -3,21 +3,21 @@ public class TestEmployee {
         double e1 = E1.salary();
         double e2 = E2.salary();
         if(e1<1200 && E1.getyears() >= 5){
-            System.out.println("O ergazomenos "+E1.getfullName()+" einai xamilomisthos");
+            System.out.println("Employee "+E1.getfullName()+" is low-paid");
             E1.setbaseSalary(E1.getbaseSalary() + 100);
-            System.out.println("Nea stoixeia: \n" + E1);
+            System.out.println("Updated information: \n" + E1);
             System.out.println("============================================================");
         }
         if(e2<1200 && E2.getyears() >= 5){
-            System.out.println("O ergazomenos "+E2.getfullName()+" einai xamilomisthos");
+            System.out.println("Employee "+E2.getfullName()+" is low-paid");
             E1.setbaseSalary(E2.getbaseSalary() + 100);
-            System.out.println("Nea stoixeia: \n" + E2);
+            System.out.println("Updated information: \n" + E2);
             System.out.println("=============================================================");
         }
     }
     public static void main(String[] args) {
-        Employee E1= new Employee (12345, "Moumoulidou Maria", 900 , 5.0 , 100);
-        Employee E2= new Employee (23456, "Andritsoudi Despoina", 1300 , 2.5 , 50);
+        Employee E1= new Employee (12345, "Employee1", 900 , 5.0 , 100);
+        Employee E2= new Employee (23456, "Employee2", 1300 , 2.5 , 50);
         System.out.println(E1);
         System.out.println();
         System.out.println(E2);
