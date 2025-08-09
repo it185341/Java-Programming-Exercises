11cdf42af0f0c2946339a21f23da0384af569fd0
Maria Moumoulidou <149332478+it185341@users.noreply.github.com>
2025-08-09 22:19:43 +0300

    Create MyUtils.java

---
+++ Lab 4: Sequential and Binary Search/Εxercise1/MyUtils.java
@@ -0,0 +1,9 @@
+public class MyUtils {
+        public static int SearchLsName(Student[] S,String F){
+        for(int i=0;i<S.length;i++){ 
+            if(S[i].getLsName().equals(F))  
+                return i; //
+        }
+        return -1; // if the student is not found
+    }
+}
