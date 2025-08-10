03ad3e22ed5ad51e8bb1c703f03fa4467006e45d
Maria Moumoulidou <149332478+it185341@users.noreply.github.com>
2025-08-10 23:57:45 +0300

    Create Student.java

---
+++ Lab 6: Quick Sort & Merge Sort/Exercise2_Merge_Sort/Student.java
@@ -0,0 +1,54 @@
+public class Student {
+    private String name; //Student’s first name
+    private String lsName; //Student's last name
+    private short id; 
+    private short year; // student’s year admission
+    
+    
+    public Student(){} //Default Constructor
+    // Constructor that sets the student's first and last name
+    public Student(String name,String lsName){
+        this.name = name;
+        this.lsName = lsName; 
+    }
+    //Full Constructor
+    public Student(String name,String lsName,short id, short year){
+        this.name = name;
+        this.lsName = lsName;
+        this.id = id;
+        this.year = year;     
+    }
+    
+    // ————Setters & Getters————————
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    
+    public void setLsName(String lsName){
+        this.lsName = lsName;
+    }
+    public String getLsName(){
+        return lsName;
+    }
+    
+    public void setId(short id){
+        this.id = id;
+    }
+    public short getId(){
+        return id;
+    }
+    
+    public void setYear(short year){
+        this.year = year;
+    }
+    public short getYear(){
+        return year;
+    }
+    
+    public String toString(){
+        return("First Name: "+name+"\nLast Name: "+lsName+"\nID: "+id+"\nAdmission year: "+year);
+    }
+}
