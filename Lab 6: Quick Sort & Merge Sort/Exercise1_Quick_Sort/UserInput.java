1896035be2015d21111e7030fbc4698195726cb0
Maria Moumoulidou <149332478+it185341@users.noreply.github.com>
2025-08-10 23:43:54 +0300

    Create UserInput.java

---
+++ Lab 6: Quick Sort & Merge Sort/Exercise1_Quick_Sort/UserInput.java
@@ -0,0 +1,82 @@
+
+import java.io.*;
+class UserInput { 
+ public static String getString() { 
+String line;
+InputStreamReader isr = new InputStreamReader(System.in);
+BufferedReader br = new BufferedReader(isr);
+try {
+line=br.readLine();
+return line;
+}
+catch(Exception e) {
+return "Exception - Lathos";
+}
+ }
+ public static int getInteger() { 
+String line;
+InputStreamReader isr = new InputStreamReader(System.in);
+BufferedReader br = new BufferedReader(isr);
+try {
+line=br.readLine();
+int i=Integer.parseInt(line);
+return i;
+}
+ catch(Exception e) {
+ return -1;
+ }
+ }
+ public static short getShort() { 
+String line;
+InputStreamReader isr = new InputStreamReader(System.in);
+BufferedReader br = new BufferedReader(isr);
+try {
+line = br.readLine();
+short i = Short.parseShort(line);
+return i;
+}
+ catch(Exception e) {
+ return (short)-1;
+ }
+ }
+ public static long getLong() { 
+String line;
+InputStreamReader isr = new InputStreamReader(System.in);
+BufferedReader br = new BufferedReader(isr);
+try {
+line = br.readLine();
+long i = Long.parseLong(line);
+return i;
+}
+ catch(Exception e) {
+ return -1L;
+ }
+ }
+ public static float getFloat() { 
+String line;
+InputStreamReader isr = new InputStreamReader(System.in);
+BufferedReader br = new BufferedReader(isr);
+try {
+line = br.readLine();
+float f = Float.parseFloat(line);
+return f;
+}
+ catch(Exception e) {
+ return -1f;
+ }
+ }
+ public static double getDouble() { 
+String line;
+InputStreamReader isr = new InputStreamReader(System.in);
+BufferedReader br = new BufferedReader(isr);
+try {
+line = br.readLine();
+double d = Double.parseDouble(line);
+return d;
+}
+ catch(Exception e) {
+ return -1.0;
+}
+
+}
+}
