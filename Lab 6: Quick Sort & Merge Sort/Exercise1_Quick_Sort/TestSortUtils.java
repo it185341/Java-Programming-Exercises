74f42bfe1c14ee5f3322cdeffa7eb208b02cea03
Maria Moumoulidou <149332478+it185341@users.noreply.github.com>
2025-08-10 23:17:08 +0300

    Add files via upload

---
+++ Lab 6: Quick Sort & Merge Sort/Exercise1_Quick_Sort/TestSortUtils.java
@@ -0,0 +1,60 @@
+
+public class TestSortUtils {
+
+    public static void main(String[] args) {
+        int n=7; // Number of students
+        Student [] S = new Student[n]; // Array to store Student objects
+        // Input first name and last name for each student during object creation
+        System.out.println("Insert details for students\n");
+        for(int i = 0; i<n;i++){
+            System.out.print("First name: ");
+            String name = UserInput.getString();
+            System.out.print("Last name: ");
+            String lastName = UserInput.getString();
+            Student s = new Student(name,lastName);
+            S[i] = s;
+        }
+        // Input remaining student data: ID and admission year, using setters
+        System.out.println("Enter the remaining details for the students: ");
+        for(int i=0; i<n;i++){
+            Student s = new Student();
+            System.out.println("Student "+i);
+            System.out.print("Id: ");
+            short id = UserInput.getShort();
+            S[i].setId(id);
+            System.out.print("Year of admission: ");
+            S[i].setYear(UserInput.getShort());
+        }
+        // Call the method to sort the array of Students by last name
+        MyUtils.selectSortLastName(S);
+        // Loop through the sorted array and print each student's details
+        System.out.println("========================================");
+        System.out.println("Sorted list of students by last name ");
+        System.out.println("========================================");
+        for(int i =0;i<S.length;i++){
+            System.out.println("---------------- Student "+i+" ------------------");
+            System.out.println(S[i]); // Print the Student object's details 
+        }
+        // Call the method to sort the array of Students by year of enrollment
+        MyUtils.bubbleSortEE(S);
+        // Print a message indicating that the students are sorted by year of enrollment
+        System.out.println("========================================");
+        System.out.println("Sorted list of students by year of admission: ");
+        System.out.println("========================================");
+        // Loop through the sorted array and print each student's details
+        for(int i =0;i<S.length;i++){
+            System.out.println("---------------- Student "+i+" ------------------");
+            System.out.println(S[i]); // Print the Student object's details 
+        }
+        System.out.println("========================================");
+        System.out.println("Student data sorted by Student ID: ");
+        System.out.println("========================================");
+        MyUtils.quickSort(S); //Sort the entire array S using QuickSort algorithm by student ID
+        // Loop through the array and print each student's details
+        for(int i=0;i<S.length;i++){
+            System.out.println("---------------- Student "+i+"---------------");
+            System.out.println(S[i]); // Print student details
+        }
+    }
+    
+}
